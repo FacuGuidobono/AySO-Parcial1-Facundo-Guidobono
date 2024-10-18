96b3ab58f47b028dce2ab8c80fb7762450fd053f
Facu Guidobono <62395586+FacuGuidobono@users.noreply.github.com>
2024-10-18 00:21:27 -0300

    Update puntoe.sh

--- parcial1/puntoe.sh
+++ parcial1/puntoe.sh
@@ -7,7 +7,7 @@ cd  ~/AySO-Parcial1-Facundo-Guidobono/parcial1
 touch Filtro_Basico.txt
 
 #vuelco la info de la ram en el archivo
-head -n 1 /proc/meminfo > ~/AySO-Parcial1-Facundo-Guidobono/parcial1Filtro_Basico.txt
+head -n 1 /proc/meminfo > ~/AySO-Parcial1-Facundo-Guidobono/parcial1/Filtro_Basico.txt
 
 #agrego la informacion del chassis
 sudo dmidecode -t chassis | head -n 7 | tail -n 2 >> ~/AySO-Parcial1-Facundo-Guidobono/parcial1/Filtro_Basico.txt
